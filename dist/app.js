034a7ea1dd70a4940ef4125a35cfa85668d10ecd
Anisul Islam <anisulislam.pranto@gmail.com>
2025-07-22 18:27:09 +0600

    cache cleared

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,6 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const user_route_1 = require("./app/modules/user/user.route");
 const app = (0, express_1.default)();
-// parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use('/api/users', user_route_1.UserRoutes);
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors'
 import { UserRoutes } from './app/modules/user/user.route'
 const app: Application = express()
 
-// parsers
 app.use(express.json())
 app.use(cors())
 
