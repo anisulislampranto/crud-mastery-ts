6982d7289f31637614883d225d01ea105c2fe02e
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-09-12 22:08:08 +0600

    joi has no built in method to check unique

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -5,7 +5,7 @@ import studentValidationSchema from "./student.validation";
 const createStudent = async (req: Request, res: Response) => {
     const { student: studentData } = req.body;
 
-    const { error } = studentValidationSchema.validate(studentData);
+    const { error, value } = studentValidationSchema.validate(studentData);
 
     if (error) {
         res.status(500).json({
@@ -16,7 +16,7 @@ const createStudent = async (req: Request, res: Response) => {
     }
 
     try {
-        const result = await StudentServices.createStudentIntoDB(studentData)
+        const result = await StudentServices.createStudentIntoDB(value)
 
         res.status(200).json({
             success: true,
@@ -24,7 +24,11 @@ const createStudent = async (req: Request, res: Response) => {
             data: result
         })
     } catch (error) {
-
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create student',
+            error: error
+        })
     }
 }
 
